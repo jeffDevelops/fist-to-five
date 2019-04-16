8e5535fa55fd76afec8a1ad00a8e19b5a97cc488
jeffDevelops <jefferydreynolds@gmail.com>
2019-04-15 18:23:39 -0600

    configures production environment

--- client/src/environments/determineEnv.js
+++ client/src/environments/determineEnv.js
@@ -1,6 +1,7 @@
 export default function() {
   switch(window.location.host) {
     case 'localhost:3000': return 'DEV';
+    case 'trilogy-fist-to-five.herokuapp.com': return 'PROD';
     // TODO: other environments here
     default: throw new Error('Environment not configured in environments/determineEnv');
   }
--- client/src/environments/determineServerHostname.js
+++ client/src/environments/determineServerHostname.js
@@ -3,6 +3,7 @@ import determineEnv from './determineEnv';
 export default function() {
   switch (determineEnv()) {
     case 'DEV': return 'http://localhost:3001';
+    case 'PROD': return 'https://trilogy-fist-to-five.herokuapp.com';
     // Other environements here
     default: throw new Error('Environment not configured in determineServerHostname');
   }
